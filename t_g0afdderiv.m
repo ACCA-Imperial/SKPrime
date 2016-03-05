4134b37b73a3e5887631df6b4b9fbecb89c78fae
Everett Kropf <ehkropf@gmail.com>
2016-03-05 10:49:41 +0000

    Update test file.

--- t_g0afdderiv.m
+++ t_g0afdderiv.m
@@ -1,35 +1,71 @@
-%% G0 alpha FD derivative.
+%% Gj alpha FD derivative.
 clear
 
-D = dat_test_data(3);
+dv = [
+    -0.2517+0.3129i
+    0.2307-0.4667i
+    ];
+qv = [
+    0.2377
+    0.1557
+    ];
+D = skpDomain(dv, qv);
 alpha = 0.5;
 
 h = 1e-6;
 
-g0 = greensC0(alpha, D);
+
+%%
+
+j = 0;
+if j == 0
+    gfun = @greensC0;
+else
+    gfun = @(a, D) greensCj(a, j, D);
+end
+
+gj = gfun(alpha, D);
+
+da = alpha + h*[-1, 1]/2;
+gh1 = gfun(da(1), gj);
+gh2 = gfun(da(2), gj);
+
+% Alpha derivative of Gj.
+dagj = @(z) (gh2(z) - gh1(z))/h;
+
+% Alpha derivative of Gjhat.
+dagjh = @(z) (gh2.hat(z) - gh1.hat(z))/2/h;
 
 
 %%
 
-da = alpha + h*[1, -1, 1i, -1i]/2;
-
-j = 1;
-drt = greensCj(da(1), j, D);
-dlf = greensCj(da(2), j, drt);
-dup = greensCj(da(3), j, drt);
-ddn = greensCj(da(4), j, drt);
-
-dgj = @(z) (drt.hat(z) - dlf.hat(z) + dup.hat(z) - ddn.hat(z))/h;
-
-
-%%
-
-% plotfd(dg0, D, 'unit')
-
-
-%%
-
-zb = boundaryPts(D, 50);
+[zb, t] = boundaryPts(D, 200);
 zb = zb(:,1:3);
 
-imag(dgj(zb)) + real(1./(zb - alpha))/(2*pi)
+clf
+% plot(t, abs(real(1./(zb - alpha))/2/pi - ...
+%     imag(dagj(zb)) - imag(dagjh(zb))) )
+plot(t, imag(dagjh(zb)), '-')
+hold on
+% set(gca, 'colororderindex', 1)
+plot(t, real(1./(zb - alpha))/2/pi, '--')
+hold off
+xlim([0, 2*pi])
+
+
+%%
+
+return
+
+% Points on D_zeta circles.
+zb = boundaryPts(D, 25);
+zb = zb(:,1:3);
+
+% Constant.
+disp(imag(gj(zb)))
+
+% Also constant.
+% disp(imag(dagj(zb)))
+
+% NOT CONSTANT !?!?
+disp(imag(dagjh(zb)) + real(1./(zb - alpha))/2/pi)
