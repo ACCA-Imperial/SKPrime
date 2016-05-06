9e912165c8a75f43db15602de8c385c47e3813bf
Everett Kropf <ehkropf@gmail.com>
2016-05-06 15:01:38 +0100

    Remove from running.

--- +skpUnitTest/primeNew.m
+++ +skpUnitTest/primeNew.m
@@ -19,23 +19,23 @@ classdef primeNew < matlab.unittest.TestCase
 % along with SKPrime.  If not, see <http://www.gnu.org/licenses/>.
 
 properties(ClassSetupParameter)
-    domain = struct('simple', skpUnitTest.domainSimple);
+    domain = struct('simple3', skpUnitTest.domainSimple3);
 end
 
-properties
-    wprod
-end
-
-methods(TestClassSetup)
-    function ClassSetup(test, domain)
-        test.wprod = skprod(domain.dv, domain.qv, 6);
-    end
-end
-
-methods(Test)
-    function checkViaSKProd(test)
-        
-    end
-end
+% properties
+%     wprod
+% end
+% 
+% methods(TestClassSetup)
+%     function ClassSetup(test, domain)
+%         test.wprod = skprod(domain.dv, domain.qv, 6);
+%     end
+% end
+% 
+% methods(Test)
+%     function checkViaSKProd(test)
+%         
+%     end
+% end
 
 end
