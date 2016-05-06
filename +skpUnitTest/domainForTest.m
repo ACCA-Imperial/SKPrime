647cc49f084bdef04fe347c74e8807dde3ee99b8
Everett Kropf <ehkropf@gmail.com>
2016-05-06 10:39:01 +0100

    Add test point.

--- +skpUnitTest/domainForTest.m
+++ +skpUnitTest/domainForTest.m
@@ -1,15 +1,29 @@
 classdef(Abstract) domainForTest
 %domainForTest base test domain abstract class.
 
+% Everett Kropf, 2016
+% 
+% This file is part of SKPrime.
+% 
+% SKPrime is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% SKPrime is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with SKPrime.  If not, see <http://www.gnu.org/licenses/>.
+
 properties(Abstract)
     dv
     qv
     
     parameterInside
-end
-
-properties(Dependent)
-    parameterOutside
+    testPointInside
 end
 
 methods
@@ -23,17 +37,24 @@ methods
                 a = td.parameterInside;
                 
             case 'outside'
-                a = td.parameter.Outside;
+                a = 1/conj(td.parameterInside);
                 
             otherwise
                 a = nan;
         end
     end
-end
-
-methods %getters
-    function a = get.parameterOutside(td)
-        a = 1/conj(td.parameterInside);
+    
+    function tp = testPoint(td, isat)
+        switch isat
+            case 'inside'
+                tp = td.testPointInside;
+                
+            case 'outside'
+                tp = 1/conj(td.testPointInside);
+                
+            otherwise
+                tp = nan;
+        end
     end
 end
 
