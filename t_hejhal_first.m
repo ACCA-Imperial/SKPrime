7d52004a423fd289df08b1c67c643d4c9cfd0091
Everett Kropf <ehkropf@gmail.com>
2016-06-22 15:09:55 +0100

    Gets parameter in (1st level copies) in inner and outer holes.

--- t_hejhal_first.m
+++ t_hejhal_first.m
@@ -2,6 +2,7 @@
 clear
 
 j = 1;
+L = 6;
 
 
 %%
@@ -15,60 +16,72 @@ qv = [
 D = skpDomain(dv, qv);
 
 % alpha = 0.53528 + 0.17493i;
-alpha = -0.66122+0.28688i;
+prealpha = 1/conj(-0.66122+0.28688i);
 % alpha = dv(1) + qv(1);
 
-theta = @(j,z) dv(j) + qv(j)^2*z./(1 - conj(dv(j))*z);
+theta = @D.theta;
 
-thja = theta(j, 1/conj(alpha)); % <-------- the parameter of concern
+alpha = theta(j, 1/conj(prealpha)); % <-------- the parameter of concern
+% alpha = 1/conj(alpha);
 
 
 %%
-% Step 1: Identify which circle the parameter is in.
+% Step 1: Identify which circle the parameter is in and define the "pair
+% parameter" in the FD.
 
 try
-    thja = skpParameter(thja, D);
+    alpha = skpParameter(alpha, D);
 catch err
     fprintf('%s\n', err.message)
 end
 
-if isa(thja, 'skpParameter') && thja.state == paramState.onInnerBdry
-    fprintf('On inner boundary %d. Hejhal not needed.\n', thja.ison)
+if isa(alpha, 'skpParameter') && alpha.state == paramState.onInnerBdry
+    fprintf('On inner boundary %d. Hejhal not needed.\n', alpha.ison)
     return
 end
 
-ja = find(abs(thja - dv) <= qv);
-fprintf('Parameter is inside circle %d.\n', ja);
+if abs(alpha) < 1
+    ja = find(abs(alpha - dv) < qv + eps(2), 1);
+    if ~isempty(ja)
+        beta = theta(ja, 1/conj(alpha));
+        bcond = (isin(D, beta) || isin(D, 1/conj(beta)));
+    end
+    if isempty(ja) || ~bcond
+        fprintf('Parameter is more than one Schottky group level deep.\n')
+        return
+    end
+    fprintf('Parameter is in 1st FD copy inside circle %d.\n', ja);
+else
+    ja = find(abs(1/conj(alpha) - dv) < qv + eps(2), 1);
+    if ~isempty(ja)
+        beta = theta(ja, alpha);
+        bcond = isin(D, beta) || isin(D, 1/conj(beta));
+    end
+    if isempty(ja) || ~bcond
+        fprintf('Parameter is more than one Schottky group level deep.\n')
+        return
+    end
+    fprintf('Parameter is in 1st FD copy inside circle %d''.\n', ja);
+end
 
 
 %%
-% Step 2: Define the "pair parameter" beta in the domain, and find the prime
-% function for this parameter.
+% Step 3: Use the secondary parameter to define the prime function for the
+% main parameter.
 
-beta = 1/conj(theta(ja, 1/conj(thja)));
-
-if ~(isin(D, beta) || isin(D, 1/conj(beta)))
-    fprintf('Parameter is more than one Schottky group level deep.\n')
-    return
-end
-if abs(1/conj(alpha) - beta) > eps(20)
-    fprintf('Beta has unexpected value.\n')
-    return
-end
-
-wb = skprime(beta, dv, qv);
-
-
-%%
-% Step 3: Use this to define the prime function for alpha via the Hejhal
-% transformation.
-
+beta = 1/conj(beta);
+wb = skprime(beta, dv ,qv);
 vj = wb.vjFuns{ja};
 taujj = vj.taujj;
 rootHj = @(z) -exp(-2i*pi*(vj(beta) - vj(z) + taujj/2)) ...
     *qv(ja)/(1 - conj(dv(j))*beta);
 
-wja = @(z) wb(z).*rootHj(z);
+if abs(alpha) < 1
+    wja = @(z) wb(z).*rootHj(z);
+else
+    wja = @(z) -(z/conj(theta(ja, beta))) ...
+        .*conj(wb(1./conj(z)).*rootHj(1./conj(z)));
+end
 
 % clf
 % plotfd(wja, D, 'auto')
@@ -77,7 +90,7 @@ wja = @(z) wb(z).*rootHj(z);
 %%
 % Check using product formula.
 
-wref = skprod(dv, qv, 6);
+wref = skprod(dv, qv, L);
 
 % figure(1), clf
 % plotfd(@(z) wref(z, thja)./wref(z, 1/conj(alpha)), D, 'auto')
@@ -85,28 +98,28 @@ wref = skprod(dv, qv, 6);
 % plotfd(rootHj, D, 'auto')
 
 zp = 0.5;
-err = wref(zp, theta(j, 1/conj(alpha))) - wja(zp);
+err = wref(zp, alpha) - wja(zp);
 disp(err)
 
 
 %%
 % Check using Gj?
 
-gjref = greensCj(alpha, j, wb);
-egjref = @(z) exp(2i*pi*gjref(z));
-
-wa = skprime(alpha, wb);
-egjan = @(z) wa(z)./wja(z)*qv(j)/abs(alpha - dv(j));
-gjan = @(z) log(egjan(z))/2i/pi;
-
-% figure(1), clf
-% plotfd(gjref, D, 'unit')
-% figure(2), clf
-% plotfd(gjan, D, 'unit')
-
-zp = 0.5;
-err = gjref(zp) - gjan(zp);
-disp(err)
+% gjref = greensCj(alpha, j, wb);
+% egjref = @(z) exp(2i*pi*gjref(z));
+% 
+% wa = skprime(alpha, wb);
+% egjan = @(z) wa(z)./wja(z)*qv(j)/abs(alpha - dv(j));
+% gjan = @(z) log(egjan(z))/2i/pi;
+% 
+% % figure(1), clf
+% % plotfd(gjref, D, 'unit')
+% % figure(2), clf
+% % plotfd(gjan, D, 'unit')
+% 
+% zp = 0.5;
+% err = gjref(zp) - gjan(zp);
+% disp(err)
 
 
 
