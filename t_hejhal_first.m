5fc6ea68fc3b62380f5709215b9164027960b7d8
Everett Kropf <ehkropf@gmail.com>
2016-06-23 11:27:11 +0100

    Switch load to parameter class.

--- t_hejhal_first.m
+++ t_hejhal_first.m
@@ -1,7 +1,7 @@
 %% Hejhal identity temp code.
 clear
 
-j = 1;
+j = 2;
 L = 6;
 
 
@@ -22,7 +22,7 @@ prealpha = 1/conj(-0.66122+0.28688i);
 theta = @D.theta;
 
 alpha = theta(j, 1/conj(prealpha)); % <-------- the parameter of concern
-% alpha = 1/conj(alpha);
+alpha = 1/conj(alpha);
 
 
 %%
@@ -40,28 +40,20 @@ if isa(alpha, 'skpParameter') && alpha.state == paramState.onInnerBdry
     return
 end
 
-if abs(alpha) < 1
-    ja = find(abs(alpha - dv) < qv + eps(2), 1);
-    if ~isempty(ja)
+if alpha.state == paramState.innerDisk ...
+        || alpha.state == paramState.outerDisk
+    ja = alpha.indisk;
+    fprintf('Parameter is in 1st FD copy inside circle %d', ja);
+    if abs(alpha) < 1
         beta = theta(ja, 1/conj(alpha));
-        bcond = (isin(D, beta) || isin(D, 1/conj(beta)));
-    end
-    if isempty(ja) || ~bcond
-        fprintf('Parameter is more than one Schottky group level deep.\n')
-        return
-    end
-    fprintf('Parameter is in 1st FD copy inside circle %d.\n', ja);
-else
-    ja = find(abs(1/conj(alpha) - dv) < qv + eps(2), 1);
-    if ~isempty(ja)
+    else
+        fprintf('''')
         beta = theta(ja, alpha);
-        bcond = isin(D, beta) || isin(D, 1/conj(beta));
     end
-    if isempty(ja) || ~bcond
-        fprintf('Parameter is more than one Schottky group level deep.\n')
-        return
-    end
-    fprintf('Parameter is in 1st FD copy inside circle %d''.\n', ja);
+    fprintf('.\n')
+else
+    fprintf('Parameter is more than one Schottky group level deep.\n')
+    return
 end
 
 
@@ -99,7 +91,7 @@ wref = skprod(dv, qv, L);
 
 zp = 0.5;
 err = wref(zp, alpha) - wja(zp);
-disp(err)
+disp(abs(err))
 
 
 %%
