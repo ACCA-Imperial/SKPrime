08de9738c625f031534ecd22aa5978f3a69b0dc4
Everett Kropf <ehkropf@gmail.com>
2016-07-07 13:40:25 +0100

    Use default parameter list.

--- +skpUnitTest/primeSimple3.m
+++ +skpUnitTest/primeSimple3.m
@@ -20,7 +20,7 @@ classdef primeSimple3 < skpUnitTest.primeNewBase
 % along with SKPrime.  If not, see <http://www.gnu.org/licenses/>.
 
 properties(MethodSetupParameter)
-    parameterAt = {'inside', 'outside', 'origin', 'infinity'};
+    parameterAt = skpUnitTest.domainSimple3.parameterLocations;
 end
 
 properties
